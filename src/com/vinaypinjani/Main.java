837b8e9de0a36030e7f1b6cdbcb44cd3a6eb7b74
vinaypinjani <35225431+vinaypinjani@users.noreply.github.com>
2019-07-29 15:52:31 +0500

    Initial commit

---
+++ src/com/vinaypinjani/Main.java
@@ -0,0 +1,22 @@
+package com.vinaypinjani;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+    }
+
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long days = (minutes / 60) / 24;
+            long years = days / 365;
+            long remainingDays = days % 365;
+            System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
+        }
+    }
+}
